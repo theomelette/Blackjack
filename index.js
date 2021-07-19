5ac6c9a99d247bf6f19bda9a92b18b3190ea77b1
Amit Naipaul <amitnaipaul@gmail.com>
2021-07-19 11:16:06 -0400

    added logic

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let firstCard = 3
-let secondCard = 10
+let firstCard = 11
+let secondCard = 11
 
 
 let sum = firstCard + secondCard
-console.log(sum)
 
+
+if (sum <= 20){
+    console.log("Do you want to draw a new card? 🙂")
+}
+else if (sum === 21){
+    console.log("Wohoo! You've got Blackjack! 🥳")
+}
+
+else {
+    console.log("You're out of the game! 😭")
+}
