bac321f07f7e510bbbfa19a4979dcf3f1a3faf57
Amit Naipaul <amitnaipaul@gmail.com>
2021-07-22 16:18:18 -0400

    added logic

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,12 +9,19 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.querySelector("#cards-el");
 
+function getRandomCard() {
+    return Math.floor(Math.random() * 10 ) + 1
+}
+
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "Cards: " + cards[0] + "   " + cards[1];
+  cardsEl.textContent = "Cards: " 
+  for (let i = 0; i < cards.length; i++) {
+      cardsEl.textContent += cards[i] + " " + "-"
+  }
   sumEl.textContent = "Sum: " + sum;
 
   if (sum <= 20) {
@@ -30,7 +37,7 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 4;
+  let card = getRandomCard();
   sum += card;
   cards.push(card)
   renderGame();
