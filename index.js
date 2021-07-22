1189752de811275f3e8b289c52d4e109c0024622
Amit Naipaul <amitnaipaul@gmail.com>
2021-07-22 18:02:19 -0400

    final?

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let player = {
+  name: "Amit",
+  chips: 100
+}
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
 
 console.log(cards);
 
