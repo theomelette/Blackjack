fa2d6d21fa113fbbc17df6e2934bf4c556e3782e
Amit Naipaul <amitnaipaul@gmail.com>
2021-07-21 23:45:12 -0400

    added cards

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
 
-function startGame(){
+function startGame() {
+    renderGame()
+}
+
+
+function renderGame(){
+    cardsEl.textContent = "Cards: " + cards[0] + "   " + cards[1]
+    sumEl.textContent = "Sum: " + sum
 
     if (sum <= 20) {
         message = "Do you want to draw a new card? "
     } 
     else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! "
+        message = "You've got Blackjack! "
         hasBlackJack = true
     } 
     else {
         message = "You're out of the game! "
         isAlive = false
     }
+    messageEl.textContent = message
+}
 
-    console.log(message)
-
+function newCard() {
+    let card = 4
+    sum += card
+    renderGame()
 }
 
 
